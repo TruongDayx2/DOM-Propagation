8186688de9112780868640a9728ef41756f8006a
Lam Trường <88103040+TruongDayx2@users.noreply.github.com>
2022-07-30 17:24:54 +0700

    preventDefault
    Loại bỏ hành vi mặc định của trình duyệt lên thẻ HTML

---
+++ main.js
@@ -0,0 +1,27 @@
+// Xoá bỏ hành vi mặc định của trình duyệt trên thẻ HTML
+
+aElements = document.querySelectorAll('a');
+
+for (var i = 0; i<aElements.length; ++i){
+    aElements[i].onclick = function(e){
+        // Nếu href không bắt đầu với chuỗi trong startsWith thì ko thực hiện
+        if(!e.target.href.startsWith('https://vi-vn.facebook.com/')){
+            // Loại bỏ hành vi mặc định của trình duyệt
+            e.preventDefault();
+        }
+    }    
+}
+
+// Loại bỏ hành vi khi nhấn chuột ra khỏi vùng ô tìm kiếm của thẻ input
+ulElement = document.querySelector('ul');
+
+// Loại bỏ mặc định của thẻ ul
+// onmousedown là khi dùng tay nhấn chuột xuống và chưa thả ra
+ulElement.onmousedown = function(e){
+    e.preventDefault();
+}
+
+// click vào thẻ li trong thẻ ul 
+ulElement.onclick = function(e){
+    console.log(e.target);
+}
